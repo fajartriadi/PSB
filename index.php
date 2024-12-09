73d357577aadaaa6625da7548484de91f43a4ff9
fajartriadi <fajartriadi222@gmail.com>
2024-12-09 20:52:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,81 +3,79 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Penerimaan Siswa Baru SMK</title>
+    <title>Penerimaan Siswa Baru SMP</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
-<header>
-    <h1>Selamat Datang di Penerimaan Siswa Baru SMP Slawe</h1>
-    <nav>
-        <ul>
-            <li><a href="index.php">Beranda</a></li>
-            <li><a href="registrasi.php">Pendaftaran</a></li>
-            <li><a href="#fasilitas">Fasilitas</a></li>
-        </ul>
-        <a href="login.php" class="btn-login">Login</a>
-    </nav>
-</header>
+    <header>
+        <h1>Selamat Datang di Penerimaan Siswa Baru SMP Slawe</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Beranda</a></li>
+                <li><a href="registrasi.php">Pendaftaran</a></li>
+                <li><a href="#fasilitas">Fasilitas</a></li>
+            </ul>
+            <a href="login.php" class="btn-login">Login</a>
+        </nav>
+    </header>
 
     <section class="visi-misi">
         <div class="content-container">
             <h2>SMP Slawe</h2>
             <p><em>"Mewujudkan generasi unggul, inovatif, dan berkarakter untuk menghadapi tantangan global."</em></p>
+        </div>
     </section>
 
     <section class="fasilitas" id="fasilitas">
         <h2>Fasilitas Kami</h2>
-       
-        
         <div class="gallery">
             <div class="gallery-item">
-                <img src="https://web.alwildan.sch.id/colsigow/2023/08/language-lab-181083_1280-1080x675.webp" alt="Fasilitas 1">
+                <img src="https://web.alwildan.sch.id/colsigow/2023/08/language-lab-181083_1280-1080x675.webp" alt="Laboratorium Komputer">
                 <p>Laboratorium Komputer</p>
             </div>
             <div class="gallery-item">
-                <img src="https://i.pinimg.com/550x/ed/b6/ec/edb6ecf117ae08f2b75acdb769f807fc.jpg" alt="Fasilitas 2">
+                <img src="https://i.pinimg.com/550x/ed/b6/ec/edb6ecf117ae08f2b75acdb769f807fc.jpg" alt="Ruang Kelas Modern">
                 <p>Ruang Kelas Modern</p>
             </div>
             <div class="gallery-item">
-                <img src="https://mpn.kominfo.go.id/wp-content/uploads/2022/12/Picture4.png" alt="Fasilitas 3">
+                <img src="https://mpn.kominfo.go.id/wp-content/uploads/2022/12/Picture4.png" alt="Perpustakaan">
                 <p>Perpustakaan</p>
             </div>
             <div class="gallery-item">
-                <img src="https://smknurulislam.sch.id/wp-content/uploads/2020/06/UKS.jpg" alt="Fasilitas 3">
+                <img src="https://smknurulislam.sch.id/wp-content/uploads/2020/06/UKS.jpg" alt="Ruang UKS">
                 <p>Ruang UKS</p>
             </div>
             <div class="gallery-item">
-                <img src="https://www.tokopartisigeser.com/wp-content/uploads/2024/07/Aula-Sekolah-Serbaguna-scaled.jpg.webp" alt="Fasilitas 1">
+                <img src="https://www.tokopartisigeser.com/wp-content/uploads/2024/07/Aula-Sekolah-Serbaguna-scaled.jpg.webp" alt="Aula Sekolah">
                 <p>Aula Sekolah</p>
             </div>
             <div class="gallery-item">
-                <img src="https://smpn3-mgl.sch.id/wp-content/uploads/2024/06/WhatsApp-Image-2024-06-06-at-08.18.15-1024x768.jpeg" alt="Fasilitas 1">
-                <p>Musolah</p>
+                <img src="https://smpn3-mgl.sch.id/wp-content/uploads/2024/06/WhatsApp-Image-2024-06-06-at-08.18.15-1024x768.jpeg" alt="Musolah">
+                <p>Mushola</p>
             </div>
             <div class="gallery-item">
-                <img src="https://www.umn.ac.id/wp-content/uploads/2024/01/Indoor-Sports-Courts.webp" alt="Fasilitas 4">
+                <img src="https://www.umn.ac.id/wp-content/uploads/2024/01/Indoor-Sports-Courts.webp" alt="Lapangan Olahraga">
                 <p>Lapangan Olahraga</p>
             </div>
             <div class="gallery-item">
-                <img src="https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2022/07/27/2212559803.jpg" alt="Fasilitas 6">
+                <img src="https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2022/07/27/2212559803.jpg" alt="Kantin Sekolah">
                 <p>Kantin Sekolah</p>
             </div>
             <div class="gallery-item">
-                <img src="https://serpongupdate.com/wp-content/uploads/2019/01/parkir.jpg" alt="Fasilitas 6">
+                <img src="https://serpongupdate.com/wp-content/uploads/2019/01/parkir.jpg" alt="Tempat Parkir Kendaraan">
                 <p>Tempat Parkir Kendaraan</p>
             </div>
             <div class="gallery-item">
-                <img src="https://i.pinimg.com/550x/a7/e4/83/a7e483754a941109da766149b74c4f14.jpg" alt="Fasilitas 6">
+                <img src="https://i.pinimg.com/550x/a7/e4/83/a7e483754a941109da766149b74c4f14.jpg" alt="Toilet">
                 <p>Toilet</p>
+            </div>
+        </div>
     </section>
 
     <footer>
-    <p><em>Alamat: Panjang, Kota Bandar Lampung, Provinsi Lampung<em></p>
-    <p>&copy; <?= date('Y') ?> PPDB Online. Kelompok 7.</p>
-        <br>
+        <p><em>Alamat: Panjang, Kota Bandar Lampung, Provinsi Lampung</em></p>
+        <p>&copy; <?php echo date('Y'); ?> PPDB Online. Kelompok 7.</p>
     </footer>
-
-    
 </body>
 </html>
